f71ff8c6c0262b6ac9b524530379daec3dabc6d6
Luke Speer <luke.speer@outlook.com>
2026-09-10 18:52:05 -0500

    add locations and records.

--- justweather/lib/main.dart
+++ justweather/lib/main.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:justweather/objects/item.dart';
 import 'package:justweather/widgets/to_do_items.dart';
 import 'package:justweather/widgets/to_do_dialog.dart';
+import 'package:justweather/objects/locations.dart';
+import 'package:justweather/api/weather_service.dart';
+import 'package:justweather/widgets/weather_record.dart';
 
 class ToDoList extends StatefulWidget {
   const ToDoList({super.key});
@@ -12,72 +15,41 @@ class ToDoList extends StatefulWidget {
 }
 
 class _ToDoListState extends State<ToDoList> {
-  final List<Item> items = [const Item(name: "add more todos")];
-  final _itemSet = <Item>{};
-
-  void _handleListChanged(Item item, bool completed) {
-    setState(() {
-      // When a user changes what's in the list, you need
-      // to change _itemSet inside a setState call to
-      // trigger a rebuild.
-      // The framework then calls build, below,
-      // which updates the visual appearance of the app.
-
-      items.remove(item);
-      if (!completed) {
-        print("Completing");
-        _itemSet.add(item);
-        items.add(item);
-      } else {
-        print("Making Undone");
-        _itemSet.remove(item);
-        items.insert(0, item);
-      }
-    });
-  }
-
-  void _handleDeleteItem(Item item) {
-    setState(() {
-      print("Deleting item");
-      items.remove(item);
-    });
-  }
-
-  void _handleNewItem(String itemText, TextEditingController textController) {
-    setState(() {
-      print("Adding new item");
-      Item item = Item(name: itemText);
-      items.insert(0, item);
-      textController.clear();
-    });
-  }
+  final List<Location> locations = [
+    Location(name: "Washington, D.C.", latitude: 38.8977, longitude: -77.0365),
+    Location(name: "New York City", latitude: 40.7128, longitude: -74.0060),
+    Location(name: "Los Angeles", latitude: 34.0522, longitude: -118.2437),
+    Location(name: "Chicago", latitude: 41.8781, longitude: -87.6298),
+    Location(name: "Houston", latitude: 29.7604, longitude: -95.3698),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('To Do List'),
-        ),
-        body: ListView(
+      appBar: AppBar(
+        title: const Text('Just Weather'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {
+              
+            },
+          ),
+        ],
+      ),
+      body: NotificationListener<OverscrollIndicatorNotification>(
+        onNotification: (OverscrollIndicatorNotification overscroll) {
+          overscroll.disallowIndicator();
+          return true;
+        },
+        child: ListView(
           padding: const EdgeInsets.symmetric(vertical: 8.0),
-          children: items.map((item) {
-            return ToDoListItem(
-              item: item,
-              completed: _itemSet.contains(item),
-              onListChanged: _handleListChanged,
-              onDeleteItem: _handleDeleteItem,
-            );
+          children: locations.map((location) {
+            return WeatherRecord(location: location);
           }).toList(),
         ),
-        floatingActionButton: FloatingActionButton(
-            child: const Icon(Icons.add),
-            onPressed: () {
-              showDialog(
-                  context: context,
-                  builder: (_) {
-                    return ToDoDialog(onListAdded: _handleNewItem);
-                  });
-            }));
+      ),
+    );
   }
 }
 
---
+++ justweather/lib/widgets/weather_record.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:justweather/objects/weather_data.dart';
+import 'package:justweather/objects/locations.dart';
+import 'package:justweather/api/weather_service.dart';
+
+class WeatherRecord extends StatefulWidget {
+  const WeatherRecord({super.key, required this.location});
+
+  final Location location;
+
+  @override
+  State<WeatherRecord> createState() => _WeatherRecordState();
+}
+
+class _WeatherRecordState extends State<WeatherRecord> {
+
+  late Future<WeatherData> weatherData;
+  @override
+  void initState() {
+    super.initState();
+    weatherData = getWeather(latitude: widget.location.latitude, longitude: widget.location.longitude);
+  }
+
+  Widget build(BuildContext context) {
+    return Card(
+      child: FutureBuilder<WeatherData>(
+        future: weatherData,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const CircularProgressIndicator();
+          } else if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          } else if (snapshot.hasData) {
+            final data = snapshot.data!;
+            return ListTile(
+              title: Text(widget.location.name),
+              subtitle: Text('State: ${data.region}, H: ${data.high}, L: ${data.low}'),
+            );
+          } else {
+            return const Text('No data available');
+          }
+        },
+      )
+    );
+  }
+}
